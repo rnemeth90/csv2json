c33feb48e2404a9993d58fb1add1522eda1f3b85
Ryan Nemeth <ryannemeth@live.com>
2022-09-08 18:42:12 -0400

    working

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type file struct {
@@ -16,7 +20,8 @@ func main() {
 		path: "./questions.csv",
 	}
 	result, _ := readCsvFile(f)
-	fmt.Println(result)
+	json := parseToJson(result)
+	fmt.Println(string(json))
 }
 
 func readCsvFile(f file) ([][]string, error) {
@@ -29,6 +34,55 @@ func readCsvFile(f file) ([][]string, error) {
 	check(err)
 
 	return records, nil
+}
+
+func parseToJson(records [][]string) []byte {
+
+	if len(records) < 1 {
+		log.Fatal("Something wrong, the file maybe empty or length of the lines are not the same")
+	}
+
+	headers := make([]string, 0)
+	for _, v := range records[0] {
+		headers = append(headers, v)
+	}
+
+	// Remove the header
+	records = records[1:]
+
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	for i, d := range records {
+		buffer.WriteString("{")
+		for j, y := range d {
+			buffer.WriteString(`"` + headers[j] + `":`)
+			_, fErr := strconv.ParseFloat(y, 32)
+			_, bErr := strconv.ParseBool(y)
+			if fErr == nil {
+				buffer.WriteString(y)
+			} else if bErr == nil {
+				buffer.WriteString(strings.ToLower(y))
+			} else {
+				buffer.WriteString((`"` + y + `"`))
+			}
+			//end of property
+			if j < len(d)-1 {
+				buffer.WriteString(",")
+			}
+
+		}
+		//end of object of the array
+		buffer.WriteString("}")
+
+		if i < len(records)-1 {
+			buffer.WriteString(",")
+		}
+	}
+	buffer.WriteString(`]`)
+
+	rawMessage := json.RawMessage(buffer.String())
+	x, _ := json.MarshalIndent(rawMessage, "", "  ")
+	return x
 }
 
 func check(err error) {
