3533edc15a0e463d5ea4a7f1f925916ea2a55cd9
Ryan Nemeth <ryannemeth@live.com>
2023-01-06 14:07:27 -0500

    changes for new tag r1.0.4

--- create_new_gh_release.sh
+++ create_new_gh_release.sh
@@ -6,18 +6,27 @@
 
 # check for Go Lang compiler and Github CLI binaries
 go_bin=$(which go)
-[ -n "$go_bin" ] || { echo "ERROR GoLang binary not available, install it using `sudo apt install go`"; exit 2; }
+[ -n "$go_bin" ] || {
+  echo "ERROR GoLang binary not available, install it using $(sudo apt install go)"
+  exit 2
+}
 gh_bin=$(which gh)
-[ -n "$gh_bin" ] || { echo "ERROR Github CLI binary not available, https://github.com/cli/cli/blob/trunk/docs/install_linux.md"; exit 2; }
+[ -n "$gh_bin" ] || {
+  echo "ERROR Github CLI binary not available, https://github.com/cli/cli/blob/trunk/docs/install_linux.md"
+  exit 2
+}
 
 #
 # CALCULATE NEW SEMANTIC TAG (vX.Y.Z)
 #
-if git tag --sort=committerdate | grep -q ^r ; then
+if git tag --sort=committerdate | grep -q ^r; then
 
   # get latest semantic version tag, construct patch+1
   semantic_version=$(git tag --sort=-committerdate | grep ^r | grep -Po '^r[0-9]*.[0-9]*.[0-9]*' | head -n1)
-  [ -n "$semantic_version" ] || { echo "ERROR could not find semantic version rX.Y.Z"; exit 3; }
+  [ -n "$semantic_version" ] || {
+    echo "ERROR could not find semantic version rX.Y.Z"
+    exit 3
+  }
 
   major_minor=$(echo "$semantic_version" | cut -d'.' -f1-2)
   patch=$(echo "$semantic_version" | cut -d'.' -f3)
@@ -35,7 +44,7 @@ echo "old version: $semantic_version new_version: ${newtag}"
 CUR_DIR=${PWD##*/}
 
 mkdir -p build
-cp src/main.go build/main.go
+cp main.go build/main.go
 cd build
 [ -f go.mod ] || go mod init rnemeth90/$CUR_DIR
 go mod tidy
@@ -45,18 +54,18 @@ set +x
 cd ..
 echo "GoLang binary built as build/main"
 
-
 #
 # GENERATE RELEASE NOTES FROM GIT COMMITS
 #
 if [ -n "$semantic_version" ]; then
-  git log HEAD...${semantic_version} --pretty="- %s " > /tmp/$newtag.log
-  [ $? -eq 0 ] || { echo "ERROR could not retrieve logs for 'git log HEAD...${semantic_version}"; exit 7; }
+  git log HEAD...${semantic_version} --pretty="- %s " >/tmp/$newtag.log
+  [ $? -eq 0 ] || {
+    echo "ERROR could not retrieve logs for 'git log HEAD...${semantic_version}"
+    exit 7
+  }
 else
-  git log --pretty="- %s " > /tmp/$newtag.log
+  git log --pretty="- %s " >/tmp/$newtag.log
 fi
-
-
 
 #
 # PUSH, COMMIT NEW TAG, CREATE RELEASE
